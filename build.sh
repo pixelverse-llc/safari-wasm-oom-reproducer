9a6762a1b117eb1cca94c0ab882e9d944caf6001
Alex Suzuki <alex@pixelverse.ch>
2024-06-19 07:45:43 +0200

    build: adapted build script to use more conservative defaults (no memory growth)

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -x
+# see: https://emscripten.org/docs/tools_reference/settings_reference.html
 emcc \
     -sENVIRONMENT=web \
-    -sINITIAL_MEMORY=67108864 \
-    -sALLOW_MEMORY_GROWTH=1 \
+    -sINITIAL_MEMORY=64MB \
     -sABORTING_MALLOC=1 \
     index.c -o index.html
